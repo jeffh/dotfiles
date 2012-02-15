ba415c3fa661e8d45eafadec0ff4e4214ce60802
Jeff Hui <contrib@jeffhui.net>
2012-02-15 07:14:51 -0500

    forgot to remove old vim files.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,20 +2,26 @@
 set -e
 
 function main {
-    git submodule init
-    command_t
+    download_plugins
+    build_command_t
     symlink_to_home
 }
 
-function command_t {
+function download_plugins {
+    git submodule init
+}
+
+function build_command_t {
     # build command-t
     echo "Building Command-T"
     cd vim/.vim/bundle/command-t
     rake make
+    # restore cwd
     cd ../../../..
 }
 
 function symlink_to_home {
+    echo "Symlinking to home."
     ln -s $PWD/vim/.vim $HOME/.vim
     ln -s $PWD/vim/.vimrc $HOME/.vimrc
 }
