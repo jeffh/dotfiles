a70ecb6151264d31179f0badfb8d1c06526d5f61
Jeff Hui <contrib@jeffhui.net>
2012-03-29 06:44:23 -0400

    Moved install to subcommand. Added ability to quickly update submodules and gocode vim files.

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,4 +27,43 @@ function symlink_to_home {
     ln -s $PWD/vim/.vimrc $HOME/.vimrc
 }
 
-main
+
+
+function update {
+    update_submodules
+    # install gocode files
+    echo "Re-downloading gocode vim files ..."
+    curl -L "https://raw.github.com/nsf/gocode/master/vim/autoload/gocomplete.vim" > $PWD/vim/.vim/bundle/gocomplete/autoload/gocomplete.vim
+    curl -L "https://raw.github.com/nsf/gocode/master/vim/ftplugin/go" > $PWD/vim/.vim/bundle/gocomplete/ftplugin/go.vim
+}
+
+function update_submodules {
+    echo "Updating submodules ..."
+    CWD=$PWD
+    for i in `git submodule status | cut -f3 -d ' '`
+    do
+        echo "Updating $i ..."
+        cd $CWD/$i
+        git checkout master
+        git pull
+    done
+    cd $CWD
+}
+
+
+function help {
+    echo "Usage: $0 COMMAND"
+    echo
+    echo "Commands:"
+    echo "  install   Sets up vim config. Symlinks files to home directory."
+    echo "  update    Updates all submodules & gocode. Changes the repository."
+    echo "  help      This help"
+    echo
+}
+
+case "$1" in
+    "install") main;;
+    "upgrade"|"update") update;;
+    "help") help;;
+    *) echo "Invalid command. Use '$0 help' for help";;
+esac
