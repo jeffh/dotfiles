ed9b862c6edf58bd77eca1885a8bced5906dfdae
Jeff Hui <jeff@jeffhui.net>
2016-08-16 20:20:40 -0700

    Update fish config
    Remove fzf - didn't use
    
    Remove trim_tree - use whitespace script instead

--- fish/config.fish
+++ fish/config.fish
@@ -9,8 +9,6 @@ setenv RUST_SRC_PATH $HOME/workspace/rust-1.7.0/src
 . $HOME/.config/fish/plugins/virtualfish/auto_activation.fish
 . $HOME/.config/fish/plugins/virtualfish/global_requirements.fish
 
-alias trim_tree="ack --print0 -l '[ \t]+\$' --known-types | xargs -0 -n1 perl -pi -e 's/[ \t]+\$//'"
-
 set PATH $HOME/.rbenv/bin $PATH
 set PATH $GOPATH/bin $PATH
 set PATH ~/.rbenv/shims $PATH
@@ -21,8 +19,6 @@ setenv RBENV_SHELL fish
 set -gx RBENV_ROOT /usr/local/var/rbenv
 . (rbenv init -|psub)
 
-fzf_key_bindings
-
 # Set prompt
 . ~/.config/fish/fish_prompt.fish
 . ~/.config/fish/fish_right_prompt.fish
@@ -30,3 +26,5 @@ fzf_key_bindings
 setenv SWIFTENV_ROOT "$HOME/.swiftenv"
 setenv PATH "$SWIFTENV_ROOT/bin" $PATH
 status --is-interactive; and . (swiftenv init -|psub)
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
