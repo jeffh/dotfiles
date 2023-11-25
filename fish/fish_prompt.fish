796a5a3ca83c928646c97f6d9aaf69c60b745f62
Jeff Hui <jeff@jeffhui.net>
2023-11-24 21:33:13 -0800

    Update fish prompt

--- fish/fish_prompt.fish
+++ fish/fish_prompt.fish
@@ -16,28 +16,67 @@ function _is_git_dirty
 end
 
 function fish_prompt
-  set -l cyan (set_color -o cyan)
-  set -l yellow (set_color -o yellow)
-  set -l red (set_color -o red)
-  set -l blue (set_color -o blue)
-  set -l green (set_color -o green)
-  set -l normal (set_color normal)
+  set -l last_status $status
+  set -l duration $CMD_DURATION
 
-  set -l cwd $cyan(basename (prompt_pwd))
+  set -l info (set_color -o brblack)
+  set -l infoval (set_color -o blue)
+  set -l dirpathcolor (set_color AAA)
+  set -l dircolor (set_color -o cyan)
+  set -l exitcodecolor (set_color -o red)
+  set -l normal (set_color normal)
+  set -l durationcolor (set_color -o yellow)
+
+  set -l repocleancolor (set_color -o green)
+  set -l repodirtycolor (set_color -o red)
+
+  set -l cwd $dircolor(basename (prompt_pwd))
 
   # output the prompt, left to right
 
-  # Display [venvname] if in a virtualenv
-  if set -q VIRTUAL_ENV
-      echo -n -s (set_color -o blue) 'virtualenv: ' (basename "$VIRTUAL_ENV") $normal ' '
+  # display exit code
+  if set -q last_status && test "$last_status" -ne 0
+    echo $info'=> '$exitcodecolor$last_status $normal
   end
 
+  # Display abbrev CWD
+  echo -n -s $dirpathcolor (prompt_pwd) ' '
+
+  # Display [shell] if in pyenv
+  if set -q PYENV_SHELL && test -e (which pyenv)
+      set -l pyshell (pyenv local 2>/dev/null || pyenv global)
+      if test "$pyshell" != "system"
+        echo -n -s $info'pyenv:'$infoval $pyshell $normal ' '
+      end
+  end
+
+  # Display [venvname] if in a virtualenv
+  if set -q VIRTUAL_ENV
+      echo -n -s $info'virtualenv:'$infoval (basename "$VIRTUAL_ENV") $normal ' '
+  end
+
+  # display duration
+  if test -n "$duration"
+    set -l secs (math --scale=1 $duration/1000 % 60)
+    set -l mins (math --scale=0 $duration/60000 % 60)
+    set -l hours (math --scale=0 $duration/3600000)
+
+    set -l cmd_dur ""
+    test $hours -gt 0 && set --local --append cmd_dur $hours"h"
+    test $mins -gt 0 && set --local --append cmd_dur $mins"m"
+    test $secs -gt 0 && set --local --append cmd_dur $secs"s"
+
+    if test "$cmd_dur" != ""
+      echo -n -s $info'duration:' $durationcolor $cmd_dur $info ' now:' $infoval (date '+%I:%M:%S%p') $normal
+    end
+  end
+ 
   # Add a newline
   echo -e ""
 
   # Display [nix] if in a nix shell
   if test -n "$IN_NIX_SHELL"
-     echo -n -s (set_color -o green) 'nix: ' $normal
+     echo -n -s $info'nix:'$infoval $normal
   end
 
   # Display the current directory name
@@ -48,9 +87,9 @@ function fish_prompt
     set -l git_branch '(' (_git_branch_name) ')'
 
     if [ (_is_git_dirty) ]
-      set git_info $red $git_branch " ★ "
+      set git_info $repodirtycolor $git_branch " ★ "
     else
-      set git_info $green $git_branch
+      set git_info $repocleancolor $git_branch
     end
     echo -n -s ' · ' $git_info $normal
   end
